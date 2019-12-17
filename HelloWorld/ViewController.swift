ba654b2d7dd31fce74140fa281f6730728625fb1
Srikanth <Gali_Srikanth@msrlimited.com>
2019-12-17 23:29:02 +0530

    This is my first push.

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Gali Srikanth on 17/12/19.
+//  Copyright © 2019 Sagarika. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("HelloWorld")
+    }
+}
